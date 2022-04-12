16de80c3f8d610dab6005ad46c5e748784e74923
cesar david moreno <cesardaviddev@gmail.com>
2022-04-12 12:51:57 +0200

    Crando carpeta de pruebas js

--- JS/testingJSON.js
+++ JS/testingJSON.js
@@ -1,2 +1,28 @@
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
+// Obteniendo los elementos html del formulario para leugo obtener los datos y tambien co
+
+const namee = document.querySelector("#namee");
+const emaiil = document.querySelector("#emaill");
+
+// Varibales para almacenar los valores (datos introduciodos en el input) del formulario
+
+const valorNombre = namee.value;
+const valorEmail = emaiil.value;
+
+// Evento para enviar los datos al JSON
+
+const botonFormulario = document.querySelector("#btn-Formu")
+
+botonFormulario.addEventListener("submit", fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: valorNombre,
+    body: valorEmail,
+    
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json)))
+
