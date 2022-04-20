6a499b3983ba3314a1a7752e29ae102b51ce8774
cesar david moreno <cesardaviddev@gmail.com>
2022-04-20 15:36:15 +0200

    Creando clase y constructor para slider. Script creado en HTML para que lea el imageslider js

---
+++ JS/imageSlider.js
@@ -0,0 +1,9 @@
+const sliderDiv = document.querySelector('#slider')
+
+//a Día 10 - Parte 2 - JavaScript Bootcamp CodeHouse para clase y constructor
+
+class Slider {
+    constructor('slider') {
+
+    }
+}
