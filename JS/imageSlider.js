779434dfcd89a87a96af9fb85ae5889960927319
cesar david moreno <cesardaviddev@gmail.com>
2022-04-24 12:26:12 +0200

    Debbuging descubri porque no cargaba comentando dlas dos lineas de this image

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,21 +1,18 @@
-const sliderDiv = document.querySelector('#slider')
+
 
 //a Día 10 - Parte 2 - JavaScript Bootcamp CodeHouse para clase y constructor A partir de min 50
 
 class Slider {
-    constructor(slider,prueba) {
-        this.next = slider;
-        this.pruebilla = prueba || 'No lee pruebilla';
+    constructor(id) {
+        this.slider = document.getElementById(id);
+/*         this.images = this.slider.getElementsByTagName("img");
+        this.numImages = images.length; */
+        this.slider.classList.add('slider-container');
         
     }
 
-    anotherMethod() {
-        console.log(`Prueba duda bootcam ${this.next}`)
-    }
 }
 
-const prueba = new Slider('JEJEJ');
-const prieba2 = new Slider('eslaider','lelo')
+const eslaider = new Slider('slider'); // Puede declararse la instancia sin almacenar en una variable (Investigar porque)
 
-prueba.anotherMethod();
-prieba2.anotherMethod();
+/* document.addEventListener("load",eslaider); */
